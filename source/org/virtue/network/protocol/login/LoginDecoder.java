19a18915338357dca644ca99261d42a4442239ea
James <jamiemoore7746@gmail.com>
2015-10-25 19:49:30 -0500

    login responses documented

---
+++ source/org/virtue/network/protocol/login/LoginDecoder.java
@@ -0,0 +1,17 @@
+package org.virtue.network.protocol.login;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+public class LoginDecoder extends ByteToMessageDecoder {
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf buf, List<Object> out) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
