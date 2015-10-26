1d7eaf2d7715c21d91bc608db93e2fcf52a86920
James <jamiemoore7746@gmail.com>
2015-10-26 14:13:52 -0500

    test

---
+++ source/org/virtue/network/session/impl/LoginSession.java
@@ -0,0 +1,27 @@
+package org.virtue.network.session.impl;
+
+import java.io.IOException;
+
+import org.virtue.network.session.Session;
+
+import io.netty.channel.Channel;
+
+public class LoginSession extends Session {
+
+	public LoginSession(Channel channel) {
+		super(channel);
+	}
+
+	@Override
+	public void messageReceived(Object message) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
